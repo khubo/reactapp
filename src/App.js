7dc75f2d0bdaefb79078337350cc2490952d01ab
khubo(aswin) <aswinrajv@gmail.com>
2018-05-30 01:55:31 +0530

    implemented nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap'
 
 // components
 import Home from './components/home'
@@ -7,24 +18,51 @@ import Product from './components/product'
 import Analysis from './components/analysis'
 
 // load styles
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle =  () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Product Inventory</h1>
-        </header>
+        <header className='fixed-top mb-5'>
+          <Navbar color='light' light expand='sm'>
+            <NavbarBrand> Inventory</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='ml-auto' navbar>
+                <NavItem>
+                  <Link className='pr-4' to={'/'}> Add </Link>
+                </NavItem>
+                <NavItem>
+                  <Link className='pr-4' to={'/products'}> Products </Link> 
+                </NavItem>
+                <NavItem>
+                   <Link className='pr-4' to={'/analytics'}> Analytics </Link>
+                </NavItem>
 
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </header>
+        <div className='mt-5 pt-5'>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/products' component={Product} />
-          <Route path='/analysis' component={Analysis} />
+          <Route path='/analytics' component={Analysis} />
         </Switch>
-
+        </div>
       </div>
     )
   }
